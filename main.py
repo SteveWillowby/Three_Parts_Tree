2c33b315d6a6e4087105149bdc19d4175c3e0331
SteveWillowby <jhibshma@nd.edu>
2019-03-18 16:42:21 -0400

    What I believe is a working is_tuple_valid

--- main.py
+++ main.py
@@ -2,7 +2,20 @@ import networkx as nx
 from networkx import utils
 from simple_rule_miner import *
 
-G=nx.scale_free_graph(5)
+G=nx.DiGraph()
+G.add_node(0)
+G.add_node(1)
+G.add_node(2)
+G.add_node(3)
+G.add_node(4)
+G.add_node(5)
+
+G.add_edge(0,1)
+G.add_edge(1,2)
+G.add_edge(2,3)
+G.add_edge(3,4)
+G.add_edge(4,5)
+G.add_edge(5,0)
 
 rm = SimpleRuleMiner(G)
 
--- simple_rule_miner.py
+++ simple_rule_miner.py
@@ -10,12 +10,14 @@ class SimpleRuleMiner(RuleMinerBase):
         self._rule_findings = {}
 
     def determine_best_rule(self):
-        nodes = self._G.nodes()
+        nodes = list(self._G.nodes())
         for i in range(2, len(nodes)):
             indices = [j for j in range(0, i)]
             done = False
             while not done:
-                
+                t = [nodes[idx] for idx in indices]
+                if self.is_tuple_valid(t):
+                    print(t)
 
                 indices_idx = i - 1
                 indices[indices_idx] += 1
@@ -31,6 +33,69 @@ class SimpleRuleMiner(RuleMinerBase):
                         if indices[indices_idx] == -1:
                             indices[indices_idx] = indices[indices_idx - 1] + 1
                         indices_idx += 1
+
+    def is_tuple_valid(self, t):
+        ins = {}
+        outs = {}
+        counters = {}
+        for n in t:
+            counters[n] = 0
+        for n in t:
+            ins[n] = [edge[0] for edge in self._G.in_edges(n) if edge[0] not in counters]
+            ins[n].sort()
+            outs[n] = [edge[1] for edge in self._G.out_edges(n) if edge[1] not in counters]
+            outs[n].sort()
+        done_count = 0
+        while done_count < len(t):
+            done_count = 0
+            first_iter = True
+            for n in t:
+                if len(ins[n]) == 0:
+                    done_count += 1
+                    continue
+                if counters[n] >= len(ins[n]):
+                    done_count += 1
+                    curr_finished = True
+                else:
+                    curr_finished = False
+                    curr_node = ins[n][counters[n]]
+                if not first_iter:
+                    if curr_finished != prev_finished or curr_node != prev_node:
+                        print(f"Tuple {t} fails on in edges -- {curr_finished} {prev_finished} {curr_node} {prev_node}")
+                        return False
+                first_iter = False
+                prev_finished = curr_finished
+                prev_node = curr_node
+
+                counters[n] += 1
+
+        for n in t:
+            counters[n] = 0
+        done_count = 0
+        while done_count < len(t):
+            done_count = 0
+            first_iter = True
+            for n in t:
+                if len(outs[n]) == 0:
+                    done_count += 1
+                    continue
+                if counters[n] >= len(outs[n]):
+                    done_count += 1
+                    curr_finished = True
+                else:
+                    curr_finished = False
+                    curr_node = outs[n][counters[n]]
+                if not first_iter:
+                    if curr_finished != prev_finished or curr_node != prev_node:
+                        print(f"Tuple {t} fails on out edges")
+                        return False
+                first_iter = False
+                prev_finished = curr_finished
+                prev_node = curr_node
+                counters[n] += 1
+        return True
+
+
 
     def contract_valid_tuples(self, rule):
         pass

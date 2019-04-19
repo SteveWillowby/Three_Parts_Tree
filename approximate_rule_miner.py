35203319bcc0e4001e1dcd586cae57c1e94e5cbc
Steve Willowby <justus.i.hibshman@gmail.com>
2019-04-19 17:10:02 -0400

    A bunch of untested code for giving all possible rules for a tuple

--- approximate_rule_miner.py
+++ approximate_rule_miner.py
@@ -17,16 +17,16 @@ class ApproximateRuleMiner(RuleMinerBase):
 
         self.in_sets = {}
         self.out_sets = {}
-        self.both_sets = {}
+        # self.both_sets = {}
         for node in list(self._G.nodes()):
             in_set = Set([edge[0] for edge in self._G.in_edges(node)])
             out_set = Set([edge[1] for edge in self._G.out_edges(node)])
-            both_set = in_set | out_set
-            in_only_set = in_set - both_set
-            out_only_set = out_set - both_set
-            self.in_sets[node] = in_only_set# OrderedDict(sorted(in_only_set))
-            self.out_sets[node] = out_only_set# OrderedDict(sorted(out_only_set))
-            self.both_sets[node] = both_set # OrderedDict(sorted(both_set))
+            # both_set = in_set | out_set
+            # in_only_set = in_set - both_set
+            # out_only_set = out_set - both_set
+            self.in_sets[node] = in_set# OrderedDict(sorted(in_only_set))
+            self.out_sets[node] = out_set# OrderedDict(sorted(out_only_set))
+            # self.both_sets[node] = both_set # OrderedDict(sorted(both_set))
 
     def determine_best_rule(self):
         nodes = list(self._G.nodes())
@@ -77,178 +77,228 @@ class ApproximateRuleMiner(RuleMinerBase):
                 best_rule = occurrences
         return best_rule[1:len(best_rule)]
 
-    def is_tuple_valid(self, t):
-        ins = {}
-        outs = {}
-        counters = {}
-        for n in t:
-            counters[n] = 0
-        max_in_len = 0
-        max_out_len = 0
-        for n in t:
-            ins[n] = [edge[0] for edge in self._G.in_edges(n) if edge[0] not in counters]
-            ins[n].sort()
-            if len(ins[n]) > 0 and len(ins[n]) != max_in_len:
-                if max_in_len != 0:
-                    return False
-                max_in_len = len(ins[n])
-            outs[n] = [edge[1] for edge in self._G.out_edges(n) if edge[1] not in counters]
-            outs[n].sort()
-            if len(outs[n]) > 0 and len(outs[n]) != max_out_len:
-                if max_out_len != 0:
-                    return False
-                max_out_len = len(outs[n])
-        done_count = 0
-        while done_count < len(t):
-            done_count = 0
-            first_iter = True
-            for n in t:
-                if len(ins[n]) == 0:
-                    done_count += 1
-                    continue
-                if counters[n] >= len(ins[n]):
-                    done_count += 1
-                    curr_finished = True
-                else:
-                    curr_finished = False
-                    curr_node = ins[n][counters[n]]
-                if not first_iter:
-                    if curr_finished != prev_finished or curr_node != prev_node:
-                        return False
-                first_iter = False
-                prev_finished = curr_finished
-                prev_node = curr_node
-
-                counters[n] += 1
-
-        for n in t:
-            counters[n] = 0
-        done_count = 0
-        while done_count < len(t):
-            done_count = 0
-            first_iter = True
-            for n in t:
-                if len(outs[n]) == 0:
-                    done_count += 1
-                    continue
-                if counters[n] >= len(outs[n]):
-                    done_count += 1
-                    curr_finished = True
-                else:
-                    curr_finished = False
-                    curr_node = outs[n][counters[n]]
-                if not first_iter:
-                    if curr_finished != prev_finished or curr_node != prev_node:
-                        return False
-                first_iter = False
-                prev_finished = curr_finished
-                prev_node = curr_node
-                counters[n] += 1
-        return True
-
     # Thanks to Mark Rushakoff on Stack Overflow for the basis of this function. Although I might not use it.
+    """
     def powerset(self, iterable):
         "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
         s = list(iterable)
         return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+    """
+
+    def random_subset(self, iterable):
+        subset = []
+        for entity in entities:
+            if random.randint(0,1):
+                subset.append[entity]
+        return subset
 
     # Returns all valid, cheapest ways to edit the pair.
     # Or, if the pair is already valid, returns an empty array.
+    # Note that this may currently return duplicates.
     def best_options_for_pair(self, a, b):
         just_a = Set([a])
         just_b = Set([b])
 
-        in_comp = [self.in_sets[a] - just_b, self.in_sets[b] - just_a]
-        in_comp.append(in_comp[0] & in_comp[1])
-        out_comp = [self.out_sets[a] - just_b, self.out_sets[b] - just_a]
-        out_comp.append(out_comp[0] & out_comp[1])
+        in_sets = [self.in_sets[a] - just_b, self.in_sets[b] - just_a]
+        in_sets.append(in_sets[0] & in_sets[1])
+        out_sets = [self.out_sets[a] - just_b, self.out_sets[b] - just_a]
+        out_sets.append(out_sets[0] & out_sets[1])
 
-        three_in_values = [len(in_comp[0]), len(in_comp[1]), len(in_comp[0]) + len(in_comp[1]) - 2 * len(in_comp[2])]
-        three_out_values = [len(out_comp[0]), len(out_comp[1]), len(out_comp[0]) + len(out_comp[1]) - 2 * len(out_comp[2])]
+        three_in_values = [len(in_sets[0]), len(in_sets[1]), len(in_sets[0]) + len(in_sets[1]) - 2 * len(in_sets[2])]
+        three_out_values = [len(out_sets[0]), len(out_sets[1]), len(out_sets[0]) + len(out_sets[1]) - 2 * len(out_sets[2])]
 
         in_min = min(three_in_values)
         out_min = min(three_out_values)
 
         if in_min == 0 and out_min == 0:
-            # Already valid! No modifications needed. TODO: Add return value
-            return
+            # Already valid! No modifications needed. TODO: Add ID
+            return [[Set() for i in range(0,8)]]
 
-        a_in_add = Set()
-        a_in_del = Set()
-        b_in_add = Set()
-        b_in_del = Set()
-
-        a_out_add = Set()
-        a_out_del = Set()
-        b_out_add = Set()
-        b_out_del = Set()
-        single_return_value = [a_in_add, a_in_del, b_in_add, b_in_del, a_out_add, a_out_del, b_out_add, b_out_del]
+        return_values = []
 
         # Distinct possible best edit options:
         if three_in_values[0] == in_min:
             if three_out_values[0] == out_min:
                 # Delete a_in and delete a_out
-                a_in_del = in_comp[0]
-                a_out_del = out_comp[0]
+                a_in_add = Set()
+                a_in_del = in_sets[0]
+                b_in_add = Set()
+                b_in_del = Set()
+
+                a_out_add = Set()
+                a_out_del = out_sets[0]
+                b_out_add = Set()
+                b_out_del = Set()
+                return_values.append([a_in_add, a_in_del, b_in_add, b_in_del, a_out_add, a_out_del, b_out_add, b_out_del])
             if three_out_values[1] == out_min:
                 # Delete a_in and delete b_out
-                pass
+                a_in_add = Set()
+                a_in_del = in_sets[0]
+                b_in_add = Set()
+                b_in_del = Set()
+
+                a_out_add = Set()
+                a_out_del = Set()
+                b_out_add = Set()
+                b_out_del = out_sets[1]
+                return_values.append([a_in_add, a_in_del, b_in_add, b_in_del, a_out_add, a_out_del, b_out_add, b_out_del])
             if three_out_values[2] == out_min:
                 # Delete a_in and move outs to intersection
-                # There are actually 2^(out_comp[2]) ways to do this!
+                # There are actually 2^(out_sets[2]) ways to do this!
 
-                pass
+                a_only = out_sets[2] - out_sets[0]
+                b_only = out_sets[2] - out_sets[1]
+                a_only_subset = Set(random_subset(a_only)) # a will delete this
+                b_only_subset = Set(random_subset(b_only)) # b will delete this
+
+                a_in_add = Set()
+                a_in_del = in_sets[0]
+                b_in_add = Set()
+                b_in_del = Set()
+
+                a_out_add = b_only - b_only_subset # a adds what b does not delete
+                a_out_del = a_only_subset
+                b_out_add = a_only - a_only_subset # b adds what a does not delete
+                b_out_del = b_only_subset
+                return_values.append([a_in_add, a_in_del, b_in_add, b_in_del, a_out_add, a_out_del, b_out_add, b_out_del])
         if three_in_values[1] == in_min:
             if three_out_values[0] == out_min:
                 # Delete b_in and delete a_out
-                pass
+                a_in_add = Set()
+                a_in_del = Set()
+                b_in_add = Set()
+                b_in_del = in_sets[1]
+
+                a_out_add = Set()
+                a_out_del = in_sets[0]
+                b_out_add = Set()
+                b_out_del = Set()
+                return_values.append([a_in_add, a_in_del, b_in_add, b_in_del, a_out_add, a_out_del, b_out_add, b_out_del])
             if three_out_values[1] == out_min:
                 # Delete b_in and delete b_out
-                pass
+                a_in_add = Set()
+                a_in_del = Set()
+                b_in_add = Set()
+                b_in_del = in_sets[1]
+
+                a_out_add = Set()
+                a_out_del = Set()
+                b_out_add = Set()
+                b_out_del = out_sets[1]
+                return_values.append([a_in_add, a_in_del, b_in_add, b_in_del, a_out_add, a_out_del, b_out_add, b_out_del])
             if three_out_values[2] == out_min:
                 # Delete b_in and move outs to intersection
-                # There are actually 2^(out_comp[2]) ways to do this!
-                pass
+                # There are actually 2^(out_sets[2]) ways to do this!
+
+                a_only = out_sets[2] - out_sets[0]
+                b_only = out_sets[2] - out_sets[1]
+                a_only_subset = Set(random_subset(a_only)) # a will delete this
+                b_only_subset = Set(random_subset(b_only)) # b will delete this
+
+                a_in_add = Set()
+                a_in_del = Set()
+                b_in_add = Set()
+                b_in_del = in_sets[1]
+
+                a_out_add = b_only - b_only_subset # a adds what b does not delete
+                a_out_del = a_only_subset
+                b_out_add = a_only - a_only_subset # b adds what a does not delete
+                b_out_del = b_only_subset
+                return_values.append([a_in_add, a_in_del, b_in_add, b_in_del, a_out_add, a_out_del, b_out_add, b_out_del])
         if three_in_values[2] == in_min:
             if three_out_values[0] == out_min:
                 # Move ins to intersection and delete a_out
-                # There are actually 2^(in_comp[2]) ways to do this!
-                pass
+                # There are actually 2^(in_sets[2]) ways to do this!
+
+                a_only = in_sets[2] - in_sets[0]
+                b_only = in_sets[2] - in_sets[1]
+                a_only_subset = Set(random_subset(a_only)) # a will delete this
+                b_only_subset = Set(random_subset(b_only)) # b will delete this
+
+                a_in_add = b_only - b_only_subset # a adds what b does not delete
+                a_in_del = a_only_subset
+                b_in_add = a_only - a_only_subset # b adds what a does not delete
+                b_in_del = b_only_subset
+                
+                a_out_add = Set()
+                a_out_del = out_sets[0]
+                b_out_add = Set()
+                b_out_del = Set()
+                return_values.append([a_in_add, a_in_del, b_in_add, b_in_del, a_out_add, a_out_del, b_out_add, b_out_del])
             if three_out_values[1] == out_min:
                 # Move ins to intersection and delete b_out
-                # There are actually 2^(in_comp[2]) ways to do this!
-                pass
+                # There are actually 2^(in_sets[2]) ways to do this!
+
+                a_only = in_sets[2] - in_sets[0]
+                b_only = in_sets[2] - in_sets[1]
+                a_only_subset = Set(random_subset(a_only)) # a will delete this
+                b_only_subset = Set(random_subset(b_only)) # b will delete this
+
+                a_in_add = b_only - b_only_subset # a adds what b does not delete
+                a_in_del = a_only_subset
+                b_in_add = a_only - a_only_subset # b adds what a does not delete
+                b_in_del = b_only_subset
+
+                a_out_add = Set()
+                a_out_del = Set()
+                b_out_add = Set()
+                b_out_del = out_sets[1]
+                return_values.append([a_in_add, a_in_del, b_in_add, b_in_del, a_out_add, a_out_del, b_out_add, b_out_del])
             if three_out_values[2] == out_min:
                 # Move both ins and outs to their respective intersections
-                # There are actually 2^(in_comp[2] + out_comp[2]) ways to do this!
-                pass
+                # There are actually 2^(in_sets[2] + out_sets[2]) ways to do this!
 
+                a_only = in_sets[2] - in_sets[0]
+                b_only = in_sets[2] - in_sets[1]
+                a_only_subset = Set(random_subset(a_only)) # a will delete this
+                b_only_subset = Set(random_subset(b_only)) # b will delete this
 
-    def contract_valid_tuples(self, rule_with_occurrences):
-        for i in range(1, len(rule_with_occurrences)):
-            tuple = rule_with_occurrences[i]
-            nodes = {n: 1 for n in tuple}
-            in_edges = {}
-            out_edges = {}
-            all_internal_in = True
-            all_internal_out = True
-            for j in range(0, len(tuple)):
-                if all_internal_in:
-                    ie = self._G.in_edges(tuple[j])
-                    for edge in ie:
-                        if edge[0] not in nodes:
-                            all_internal_in = False
-                            in_edges[edge[0]] = 1
-                if all_internal_out:
-                    oe = self._G.out_edges(tuple[j])
-                    for edge in oe:
-                        if edge[1] not in nodes:
-                            all_internal_out = False
-                            out_edges[edge[1]] = 1
-                if not all_internal_in and not all_internal_out:
-                    break
-            self._G.remove_nodes_from(tuple[1:len(tuple)])
-            for source, ignored in in_edges.items():
-                self._G.add_edge(source, tuple[0])
-            for dest, ignored in out_edges.items():
-                self._G.add_edge(tuple[0], dest)
+                a_in_add = b_only - b_only_subset # a adds what b does not delete
+                a_in_del = a_only_subset
+                b_in_add = a_only - a_only_subset # b adds what a does not delete
+                b_in_del = b_only_subset
+
+                a_only = out_sets[2] - out_sets[0]
+                b_only = out_sets[2] - out_sets[1]
+                a_only_subset = Set(random_subset(a_only)) # a will delete this
+                b_only_subset = Set(random_subset(b_only)) # b will delete this
+
+                a_out_add = b_only - b_only_subset # a adds what b does not delete
+                a_out_del = a_only_subset
+                b_out_add = a_only - a_only_subset # b adds what a does not delete
+                b_out_del = b_only_subset
+                return_values.append([a_in_add, a_in_del, b_in_add, b_in_del, a_out_add, a_out_del, b_out_add, b_out_del])
+
+        return return_values
+
+    # Id value will be 6 bits:
+    # Bit 5: Does node x have in-edges?
+    # Bit 4: Does node x have out-edges?
+    # Bit 3: Does node x point to node y?
+    # Bit 2: Does node y have in-edges?
+    # Bit 1: Does node y have out-edges?
+    # Bit 0: Does node y point to node x?
+    def add_rule_ids(self, a, b, best_options):
+        just_a = Set([a])
+        just_b = Set([b])
+
+        in_sets = [self.in_sets[a] - just_b, self.in_sets[b] - just_a]
+        out_sets = [self.out_sets[a] - just_b, self.out_sets[b] - just_a]
+        
+        a_to_b = a in self.in_sets[b]
+        b_to_a = b in self.in_sets[a]
+
+        best_options_with_ids = []
+        for option in best_options:
+            # [a_in_add = 0, a_in_del = 1, b_in_add = 2, b_in_del = 3, a_out_add = 4, a_out_del = 5, b_out_add = 6, b_out_del = 7]
+            in_a = len((in_sets[0] - option[1]) + option[0]) > 0
+            in_b = len((in_sets[1] - option[3]) + option[2]) > 0
+            out_a = len((out_sets[0] - option[5]) + option[4]) > 0
+            out_b = len((out_sets[1] - option[7]) + option[6]) > 0
+            a_score = in_a * 4 + out_a * 2 + a_to_b
+            b_score = in_b * 4 + out_b * 2 + b_to_a
+            if a_score > b_score:
+                best_options_with_ids.append([(a_score << 3) + b_score, option])
+            else:
+                best_options_with_ids.append([(b_score << 3) + a_score, option])
+        return best_options_with_ids

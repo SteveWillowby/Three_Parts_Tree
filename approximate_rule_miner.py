d389e6ccbed7da8c4888c17f0d46b6f885d2d640
Steve Willowby <justus.i.hibshman@gmail.com>
2019-04-19 18:14:55 -0400

    Code to update where rules occur.

--- approximate_rule_miner.py
+++ approximate_rule_miner.py
@@ -29,17 +29,20 @@ class ApproximateRuleMiner(RuleMinerBase):
             self.out_sets[node] = out_set# OrderedDict(sorted(out_only_set))
             # self.both_sets[node] = both_set # OrderedDict(sorted(both_set))
 
-        self.rule_occurrences_by_pair = {}
-        self.rule_occurrences_by_id = {}
+        self.rule_occurrences_by_pair = {}  # {lesser_node_id: {greater_node_id: {rule_id: [adds/deletions]}}}
+        self.rule_occurrences_by_id = {}    # {rule_id: Set((lesser_node_id, greater_node_id))}
 
+    # This function is intended to be run just once at the start.
+    # It looks at every pair of connected nodes and finds all rules for the respective pairs.
+    # The information is stored in self.rule_occurrences_by_pair and self.rule_occurrences_by_id.
     def check_all_pairs_for_rules(self):
         nodes = list(self._G.nodes())
         nodes.sort()
-        for i in range(0, len(nodes)):
-            node_a = nodes[i]
+        for node_a in nodes:
             self.rule_occurrences_by_pair[node_a] = {}
-            for j in range(i + 1, len(nodes)):
-                node_b = nodes[j]
+            for node_b in self.in_set[node_a] | self.out_set[node_a]:
+                if node_b < node_a:
+                    continue
                 best_options_without_ids = self.best_options_for_pair(node_a, node_b)
                 unique_best_options_with_ids = self.add_rule_ids_and_filter(best_options_without_ids)
                 self.rule_occurrences_by_pair[node_a][node_b] = unique_best_options_with_ids
@@ -48,57 +51,60 @@ class ApproximateRuleMiner(RuleMinerBase):
                         self.rule_occurrences_by_id[id_num] = Set()
                     self.rule_occurrences_by_id[id_num].add((node_a, node_b))
 
-    def check_pairs_containing_ids(self, ids):
-        pass
+    # This function is run after a rule has contracted some nodes.
+    # It updates self.rule_occurrences_by_pair and self.rule_occurrences_by_id.
+    # ids contains any node whose rules may have been affected.
+    # This is O(|V|*max_degree^2)
+    def update_pairs_containing_ids(self, ids):
+        ids.sort()
+        for node_c in ids:
+            for node_d in self.in_set[node_c] | self.out_set[node_c]:
+                node_a = min(node_c, node_d)
+                node_b = max(node_c, node_d)
+                best_options_without_ids = self.best_options_for_pair(node_a, node_b)
+                unique_best_options_with_ids = self.add_rule_ids_and_filter(best_options_without_ids)
+                
+                # First delete any outdated occurrences:
+                if node_b not in self.rule_occurrences_by_pair[node_a]:
+                    self.rule_occurrences_by_pair[node_a][node_b] = {}
+                for id_num, option in self.rule_occurrences_by_pair[node_a][node_b].items():
+                    if id_num not in unique_best_options_with_ids:
+                        self.rule_occurrences_by_id[id_num].remove((node_a, node_b)) # Use discard instead?
+                # Then add new occurrences:
+                self.rule_occurrences_by_pair[node_a][node_b] = unique_best_options_with_ids
+                for id_num, option in unique_best_options_with_ids:
+                    if id_num not in self.rule_occurrences_by_id:
+                        self.rule_occurrences_by_id = Set()
+                    self.rule_occurrences_by_id.add((node_a, node_b)) # Adds if not present already.
+
+    # This function is used when a node is deleted from the graph.
+    # It deletes all rules containing node_id in self.rule_occurrences_by_*.
+    # Ugh. Unfortunately this is O(|V|).
+    def delete_node_from_rule_occurrences(self, node_id):
+        node_a = node_id
+        for node_b, rules in self.rule_occurrences_by_pair[node_a].items():
+            for rule_id, option in rules.items():
+                self.rule_occurrences_by_id[rule_id].remove((node_a, node_b))
+        del self.rule_occurrences_by_pair[node_a]
+
+        node_b = node_id
+        for node_a, dict_a in self.rule_occurrences_by_pair.items():
+            if node_b in dict_a:
+                for rule_id, option in dict_a[node_b].items():
+                    self.rule_occurrences_by_id[rule_id].remove((node_a, node_b))
+                del dict_a[node_b]
 
     def determine_best_rule(self):
-        nodes = list(self._G.nodes())
-        rule_lib = RuleLib()
-        rule_occurrences = {}
-        for i in range(2, len(nodes) + 1):
-            indices = [j for j in range(0, i)]
-            done = False
-            while not done:
-                t = [nodes[idx] for idx in indices]
-                if self.is_tuple_valid(t):
-                    rule = Rule(t, self._G, rule_lib)
-                    if rule.id() not in rule_occurrences:
-                        rule_occurrences[rule.id()] = [{node: 1 for node in t}, rule, t]
-                    else:
-                        all_new_nodes = True
-                        for node in t:
-                            if node in rule_occurrences[rule.id()][0]:
-                                all_new_nodes = False
-                                break
-                        if all_new_nodes:
-                            for node in t:
-                                rule_occurrences[rule.id()][0][node] = 1
-                            rule_occurrences[rule.id()].append(t)
-                indices_idx = i - 1
-                indices[indices_idx] += 1
-                while indices[indices_idx] == len(nodes) - (i - indices_idx) + 1:
-                    indices[indices_idx] = -1 # Note that the loop finished.
-                    if indices_idx == 0:
-                        done = True
-                        break
-                    indices_idx -= 1
-                    indices[indices_idx] += 1
-                if not done:
-                    while indices_idx < i:
-                        if indices[indices_idx] == -1:
-                            indices[indices_idx] = indices[indices_idx - 1] + 1
-                        indices_idx += 1
-            if len(rule_occurrences) > 0:
-                break
-            else:
-                print(f"Going to size {i + 1}")
-        max_len = 0
-        best_rule = []
-        for rule, occurrences in rule_occurrences.items():
-            if len(occurrences) > max_len:
-                max_len = len(occurrences)
-                best_rule = occurrences
-        return best_rule[1:len(best_rule)]
+        if self.first_round:
+            self.check_all_pairs_for_rules()
+            self.first_round = False
+        best_occ_len = 0
+        for id_num, occurrences in self.rule_occurrences_by_id:
+            if len(occurrences) > best_occ_len:
+                best_occ_len = len(occurrences)
+                best_occ = occurrences
+                best_id = id_num
+        return [best_id, best_occ]
 
     # Thanks to Mark Rushakoff on Stack Overflow for the basis of this function. Although I might not use it.
     """
@@ -315,10 +321,10 @@ class ApproximateRuleMiner(RuleMinerBase):
         best_options_with_ids = {}
         for option in best_options:
             # [a_in_add = 0, a_in_del = 1, b_in_add = 2, b_in_del = 3, a_out_add = 4, a_out_del = 5, b_out_add = 6, b_out_del = 7]
-            in_a = len((in_sets[0] - option[1]) + option[0]) > 0
-            in_b = len((in_sets[1] - option[3]) + option[2]) > 0
-            out_a = len((out_sets[0] - option[5]) + option[4]) > 0
-            out_b = len((out_sets[1] - option[7]) + option[6]) > 0
+            in_a = len((in_sets[0] - option[1]) | option[0]) > 0
+            in_b = len((in_sets[1] - option[3]) | option[2]) > 0
+            out_a = len((out_sets[0] - option[5]) | option[4]) > 0
+            out_b = len((out_sets[1] - option[7]) | option[6]) > 0
             a_score = in_a * 4 + out_a * 2 + a_to_b
             b_score = in_b * 4 + out_b * 2 + b_to_a
             if a_score > b_score:

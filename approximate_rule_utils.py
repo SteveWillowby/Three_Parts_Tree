74558f1dd4ca177c6f9614aa1b2ae83e526b4a76
Steve Willowby <justus.i.hibshman@gmail.com>
2019-05-09 14:47:16 -0400

    Some starting work on code to interpret edge types.

--- approximate_rule_utils.py
+++ approximate_rule_utils.py
@@ -1,19 +1,56 @@
 from bitstring import Bits
 from bitstring import BitArray
 
+# An edge type interpreter lets you modify a graph while abstractly speaking of edge types.
+# From:
+# "This edge connects a,b of type 1"
+# To:
+# "This edge is a directed edge from b to a"
+# or to:
+# "This edge is a bidirectional edge between b and a"
+# or to:
+# "This edge is .... whatever you code it to be interpreted as."
+class EdgeTypeInterpreter:
+    def __init__(self):
+        pass
+
+    def add_edge(G, edge_type, node_a, node_b):
+        pass
+
+    def remove_edge(G, edge_type, node_a, node_b):
+        pass
+
+# 0 = forward edges (out)
+# 1 = backward edges (in)
+class BiDirectionalEdgeTypeInterpreter(EdgeTypeInterpreter):
+    def __init__(self):
+        pass
+
+    def add_edge(G, edge_type, node_a, node_b):
+        if edge_type == 0:
+            G.add_edge(node_a, node_b)
+        elif edge_type == 1:
+            G.add_edge(node_b, node_a)
+        else:
+            print("MAJOR ERROR! INVALID EDGE TYPE %s" % edge_type)
+
+    def remove_edge(G, edge_type, node_a, node_b):
+        if edge_type == 0:
+            G.remove_edge(node_a, node_b)
+        elif edge_type == 1:
+            G.remove_edge(node_b, node_a)
+        else:
+            print("MAJOR ERROR! INVALID EDGE TYPE %s" % edge_type)
+
 class ApproximateRuleUtils:
-    
-    def cheapest_rules_for_tuple(in_edges, out_edges, t):
-        edge_types = [in_edges, out_edges]
-        return self.cheapest_rules_for_tuple_helper(edge_types, t)
+    def __init__(self, edge_type_interpreter):
+        self.edge_type_interpreter = edge_type_interpreter
 
     # Right now this is O(max_degree * |t| * 2^|t|)
-    def cheapest_rules_for_tuple_helper(edge_types, t):
+    def cheapest_rules_for_tuple(edge_types, t):
         if len(t) > 32:
             print("EXTREME ERROR. ASKING FOR A RULE OF MORE THAN 32 NODES. CANNOT DO BIT MATH.")
             exit(1)
-
-        # TODO: Add canonical node GROUP ordering by automorphism group
 
         t_set = Set(t)
         best_options_found = [[] for i in range(0, len(edge_types))]
@@ -37,7 +74,6 @@ class ApproximateRuleUtils:
                     max_possible_cost += 1
 
             best_options_found[edge_type_idx] = []
-            best_options_ids = Set()
             best_cost_found = max_possible_cost
             for i in range(0, 2**len(t)):
                 keep_nodes = Set([t[j] for j in range(0, len(t)) if (i >> j) & 1])
@@ -61,7 +97,6 @@ class ApproximateRuleUtils:
                     continue
                 if current_cost < best_cost_found:
                     best_options_found[edge_type_idx] = []
-                    best_options_ids = Set()
                     best_cost_found = current_cost
 
                 # If the loop is still here, this is a valid option. Add it to the list.
@@ -77,16 +112,7 @@ class ApproximateRuleUtils:
                     else:
                         additions += [(node, neighbor) for node in non_matches]
 
-                # Now we need to find the bits to represent this rule.
-                id_for_type = self.rule_id_for_single_edge_type(edge_type, keep_nodes, reject_nodes)
-                if id_for_type not in best_options_ids:
-                    best_options_found[edge_type_idx].append((id_for_type, deletions, additions))
-                    best_options_ids.add(id_for_type)
+                best_options_found[edge_type_idx].append((keep_nodes, deletions, additions))
 
-        # TODO: Combine edge type findings into full rules.
+        # TODO: Combine edge type findings into full rules and give them ids.
         combined_edge_type_options = []
-        
-
-    def rule_id_for_single_edge_type(edge_type, keep, reject):
-        # TODO: Implement this.
-        pass 

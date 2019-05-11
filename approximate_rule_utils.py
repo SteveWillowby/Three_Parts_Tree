a22ca5a94a85560b910d8b77ea76fd1f6efde3da
Steve Willowby <justus.i.hibshman@gmail.com>
2019-05-11 18:33:46 -0400

    Added code to generate a rule graph which will be isomorphic only to exact copies of that rule.

--- approximate_rule_utils.py
+++ approximate_rule_utils.py
@@ -20,6 +20,43 @@ class EdgeTypeInterpreter:
     def remove_edge(G, edge_type, node_a, node_b):
         pass
 
+    # edges_by_type should be the following:
+    #   [{node_0: set-of-node_a's-neighbors-with-edges-of-type-0, node_1: set...}, {node_0: set-of...-type-1, ...}, ...]
+    # 
+    # t should be a Set() of nodes that the rule consists of
+    #
+    # nodes_with_external_edges_by_type should be the following:
+    #   [set-of-nodes-with-external-edges-of-type-0, set-of-...-type-1, ...]
+    def make_rule_graph(edges_by_type, t, nodes_with_external_edges_by_type):
+        G = nx.DiGraph()
+
+        # Create a node with a self-loop for every edge type.
+        # Then chain them together with directed edges.
+        name = "type_0"
+        G.add_node(name)
+        G.add_edge(name, name)
+        for i in range(1, len(edges_by_type)):
+            prev_name = name
+            name = "type_%s" % i
+            G.add_node(name)
+            G.add_edge(name, name)
+            G.add_edge(prev_name, name)
+
+        # Now actually add the nodes in the tuple and the internal edges.
+        for node in t:
+            G.add_node(t)
+            for type_id in range(0, len(edges_by_type)):
+                for (node_a, node_b) in edges_by_type[type_id]:
+                    if (node_a in t) and (node_b in t):
+                        self.add_edge(G, type_id, node_a, node_b)
+
+        for type_id in range(0, len(edges_by_type)):
+            type_node_id = "type_%s" % type_id
+            for node in nodes_with_external_edges_by_type[type_id]:
+                G.add_edge(type_node_id, node)
+
+        return G
+
 # 0 = forward edges (out)
 # 1 = backward edges (in)
 class BiDirectionalEdgeTypeInterpreter(EdgeTypeInterpreter):
@@ -38,6 +75,35 @@ class BiDirectionalEdgeTypeInterpreter(EdgeTypeInterpreter):
         if edge_type == 0:
             G.remove_edge(node_a, node_b)
         elif edge_type == 1:
+            G.remove_edge(node_b, node_a)
+        else:
+            print("MAJOR ERROR! INVALID EDGE TYPE %s" % edge_type)
+
+# 0 = forward edge only (out)
+# 1 = backward edge only (in)
+# 2 = both directions
+class InOutBothEdgeTypeInterpreter(EdgeTypeInterpreter):
+    def __init__(self):
+        pass
+
+    def add_edge(G, edge_type, node_a, node_b):
+        if edge_type == 0:
+            G.add_edge(node_a, node_b)
+        elif edge_type == 1:
+            G.add_edge(node_b, node_a)
+        elif edge_type == 2:
+            G.add_edge(node_a, node_b)
+            G.add_edge(node_b, node_a)
+        else:
+            print("MAJOR ERROR! INVALID EDGE TYPE %s" % edge_type)
+
+    def remove_edge(G, edge_type, node_a, node_b):
+        if edge_type == 0:
+            G.remove_edge(node_a, node_b)
+        elif edge_type == 1:
+            G.remove_edge(node_b, node_a)
+        elif edge_type == 2:
+            G.remove_edge(node_a, node_b)
             G.remove_edge(node_b, node_a)
         else:
             print("MAJOR ERROR! INVALID EDGE TYPE %s" % edge_type)

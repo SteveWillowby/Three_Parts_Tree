96677aa7f702ecf96af3b3edc6e4e65b26d9dad5
Steve Willowby <justus.i.hibshman@gmail.com>
2019-05-31 14:35:06 -0400

    eh

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from simple_rule_miner import *
 from approximate_rule_miner import *
 from full_approximate_rule_miner import *
 
+size = 1023
+rule_min = 2
+rule_max = 4
 
 G=nx.DiGraph()
-
-size = 1023
-
 # Builds a binary tree:
 for i in range(1, size + 1):
     G.add_node(i)
@@ -18,7 +18,7 @@ for i in range(1, size + 1):
     if i * 2 + 1 < size + 1:
         G.add_edge(i, i*2 + 1)
 
-rm = FullApproximateRuleMiner(G, 2, 4)
+rm = FullApproximateRuleMiner(G, rule_min, rule_max)
 
 print("\nFor binary tree with %s nodes:" % size)
 while not rm.done():
@@ -49,7 +49,7 @@ for tree_idx in range(1, tree_size + 1):
     if next_graph_idx <= tree_size * ring_size:
         G.add_edge(ring_bottom, next_graph_idx)
 
-rm = FullApproximateRuleMiner(G, 2, 4)
+rm = FullApproximateRuleMiner(G, rule_min, rule_max)
 
 print("\nFor tree of size-%s rings with %s nodes:" % (ring_size, size))
 while not rm.done():
@@ -67,16 +67,31 @@ for i in range(1, size):
 G.add_edge(1, size)
 G.add_edge(size, 1)
 
-rm = FullApproximateRuleMiner(G, 2, 4)
+rm = FullApproximateRuleMiner(G, rule_min, rule_max)
 
 print("\nFor directed double-ring of size: %s" % size)
 while not rm.done():
     best_rule = rm.determine_best_rule()
     rm.contract_valid_tuples(best_rule)
 
-"""
+# Watts strogatz
+edges_per_node = 3
+G = nx.DiGraph()
+for i in range(0, size):
+    G.add_node(i)
+for i in range(0, size):
+    for j in range(1, edges_per_node + 1):
+        G.add_edge(i, (i + j) % size)
+
+rm = FullApproximateRuleMiner(G, rule_min, rule_max)
+
+print("\nFor directed watts strogatz of size: %s with %s edges per node" % (size, edges_per_node))
+while not rm.done():
+    best_rule = rm.determine_best_rule()
+    rm.contract_valid_tuples(best_rule)
+
 # Directed erdosh reyni:
-expected_num_edges = size
+expected_num_edges = size * 2
 prob_of_edge_numerator = expected_num_edges
 prob_of_edge_denominator = size * (size - 1)  # No div by 2 since we multiply by 2 because we can have bidirected edges.
 G = nx.DiGraph()
@@ -89,10 +104,9 @@ for i in range(1, size + 1):
         if random.randint(1, prob_of_edge_denominator) <= prob_of_edge_numerator:
             G.add_edge(i, j)
 
-rm = FullApproximateRuleMiner(G, 3, 3)
+rm = FullApproximateRuleMiner(G, rule_min, rule_max)
 
 print("\nFor directed erdosh-reyni with %s nodes and %s edges:" % (size, len(G.edges())))
 while not rm.done():
     best_rule = rm.determine_best_rule()
     rm.contract_valid_tuples(best_rule)
-"""

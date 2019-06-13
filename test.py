e3f8100da5e7a83f13b1e68503b9130d1b24502c
Steve Willowby <justus.i.hibshman@gmail.com>
2019-06-13 15:09:46 -0400

    Added ability to load a graph from a file.

--- test.py
+++ test.py
@@ -1,26 +1,44 @@
 import networkx as nx
 import argparse
+import os
 from full_approximate_rule_miner import *
 from test_utils import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument("rule_min", type=int, help="Minimum rule size")
 parser.add_argument("rule_max", type=int, help="Maximum rule size")
-parser.add_argument("graph_type", help="The graph type. Valid values are watts_strogats, n_tree, and n_tree_of_k_rings")
-parser.add_argument("size", type=int, help="The number of nodes in the graph.")
-parser.add_argument("n", type=int, help="A graph parameter. For watts_strogats, specifies number of neighbors. For the trees, specifies number of children.")
+parser.add_argument("graph_type", help="The graph type. Either the path to an edgelist file or one of the following: watts_strogats, n_tree, and n_tree_of_k_rings")
+parser.add_argument("-s", "--size", type=int, help="The number of nodes in the graph.")
+parser.add_argument("-n", type=int, help="A graph parameter. For watts_strogats, specifies number of neighbors. For the trees, specifies number of children.")
 parser.add_argument("-k", type=int, help="A graph parameter. For n_tree_of_k_rings, specifies the ring size.")
+parser.add_argument("-r", type=float, help="Probability that an edge gets rewired.")
 parser.add_argument("--bidirected", help="Add if the graph should be bidirected. Currently just for watts_strogatz")
 args = parser.parse_args()
+if args.graph_type == "watts_strogatz":
+    if args.k:
+        print("INFO: k was specified but is not used in watts_strogatz")
+    if not args.n:
+        print("ERROR: need parameter n for watts_strogatz. Aborting.")
+        exit(2)
+if args.graph_type == "n_tree":
+    if args.k:
+        print("INFO: k was specified but is not used in an n_tree")
+    if not args.n:
+        print("ERROR: need parameter n for n_tree. Aborting.")
+        exit(2)
+if args.graph_type == "n_tree_of_k_rings":
+    if not args.k:
+        print("ERROR: need k for n_tree_of_k_rings. Aborting.")
+        exit(2)
+    if not args.n:
+        print("ERROR: need parameter n for n_tree_of_k_rings. Aborting.")
+        exit(2)
 if args.graph_type not in ["watts_strogatz", "n_tree", "n_tree_of_k_rings"]:
-    print("ERROR: Unknown graph type %s. Aborting." % args.graph_type)
-    exit(2)
-if args.graph_type == "watts_strogatz" and args.k:
-    print("INFO: k was specified but is not used in watts_strogatz")
-if args.graph_type == "n_tree" and args.k:
-    print("INFO: k was specified but is not used in an n_tree")
-if args.graph_type == "n_tree_of_k_rings" and not args.k:
-    print("ERROR: need k for n_tree_of_k_rings. Aborting.")
+    if not os.path.isfile(args.graph_type):
+        print("ERROR: file [%s] not found. Aborting." % args.graph_type)
+        exit(2)
+if args.r is not None and (args.r < 0.0 or 1.0 < args.r):
+    print("ERROR: rewiring probability r must be in the range [0.0, 1.0]. Aborting.")
     exit(2)
 
 G = None
@@ -29,11 +47,24 @@ if args.graph_type == "n_tree":
 elif args.graph_type == "n_tree_of_k_rings":
     G = n_ary_tree_of_k_rings(args.size, args.n, args.k)
 elif args.graph_type == "watts_strogatz":
-    #rewiring_prob = 0
-    #if len(sys.argv) > 7:
-    #    rewiring_prob = float(sys.argv[7])
     bidirected = args.bidirected is not None
-    G = watts_strogatz(args.size, args.n, 0, bidirected)
+    G = watts_strogatz(args.size, args.n, bidirected)
+else:
+    G = nx.read_adjlist(args.graph_type, create_using=nx.DiGraph, nodetype=int)
+    G = nx.DiGraph(G)
+    remove_self_loops(G)
+    bidirected = 0.0
+    connections = 0.0
+    for edge in G.edges():
+        if edge[0] > edge[1]:
+            continue
+        connections += 1.0
+        if (edge[1], edge[0]) in G.edges():
+            bidirected += 1.0
+    print("Percent of connections that are bidirected: %s" % (bidirected / connections))
+
+if args.r is not None:
+    rewire_graph(G, args.r)
 
 rm = FullApproximateRuleMiner(G, args.rule_min, args.rule_max)
 
--- test_utils.py
+++ test_utils.py
@@ -36,7 +36,7 @@ def n_ary_tree_of_k_rings(size, n, k):
     return G
 
 # Note that, due to the current coding, and rewiring_prob over 0.5 will cause all edges to be rewired.
-def watts_strogatz(size, k, rewiring_prob=0, bidirected=True):
+def watts_strogatz(size, k, bidirected=True):
     G = nx.DiGraph()
     for i in range(0, size):
         G.add_node(i)
@@ -45,23 +45,21 @@ def watts_strogatz(size, k, rewiring_prob=0, bidirected=True):
             G.add_edge(i, (i + j) % size)
             if bidirected:
                 G.add_edge((i + j) % size, i)
-
-    if rewiring_prob > 0:
-        max_value = int(1.0 / rewiring_prob)
-        current_edges = list(G.edges())
-        for edge in current_edges:
-            if bidirected and edge[0] > edge[1]:
-                continue
-            if random.randint(1, max_value) == 1:
-                # Randomly pick a new place for edge[0] to point
-                new_target = edge[0]
-                while new_target == edge[0] or (edge[0], new_target) in G.out_edges(edge[0]):
-                    new_target = random.randint(0, size - 1)
-                G.add_edge(edge[0], new_target)
-                if bidirected:
-                    G.add_edge(new_target, edge[0])
-
-                G.remove_edge(edge[0], edge[1])
-                if bidirected:
-                    G.remove_edge(edge[1], edge[0])
     return G
+
+def rewire_graph(G, rewiring_prob):
+    current_edges = list(G.edges())
+    nodes = list(G.nodes())
+    for edge in current_edges:
+        if random.uniform(0.0, 0.999999999999999) < rewiring_prob:
+            # Randomly pick a new place for edge[0] to point
+            new_target = edge[0]
+            while new_target == edge[0] or (edge[0], new_target) in G.out_edges(edge[0]):
+                new_target = nodes[random.randint(0, len(nodes) - 1)]
+            G.add_edge(edge[0], new_target)
+            G.remove_edge(edge[0], edge[1])
+
+def remove_self_loops(G):
+    for node in G.nodes():
+        if (node, node) in G.edges():
+            G.remove_edge(node, node)

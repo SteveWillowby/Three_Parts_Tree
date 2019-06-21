3e167001431158ea7c0089d17161e571780916b6
SteveWillowby <jhibshma@nd.edu>
2019-06-21 14:52:53 -0400

    File for tests with r parameter.

---
+++ progressive_edge_replacement.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+r=0.0
+increment=0.2
+
+array=( $@ )
+len=${#array[@]}
+file=${array[$len-1]}
+other_args=${array[@]:0:$len-1}
+
+end="_r=$r"
+
+python test.py $other_args -r $r > "output_files/$file$end"
+
+r=$(echo $r + $increment | bc)
+end="_r=$r"
+
+python test.py $other_args -r $r > "output_files/$file$end"
+
+r=$(echo $r + $increment | bc)
+end="_r=$r"
+
+python test.py $other_args -r $r > "output_files/$file$end"
+
+r=$(echo $r + $increment | bc)
+end="_r=$r"
+
+python test.py $other_args -r $r > "output_files/$file$end"
+
+r=$(echo $r + $increment | bc)
+end="_r=$r"
+
+python test.py $other_args -r $r > "output_files/$file$end"
+
+r=$(echo $r + $increment | bc)
+end="_r=$r"
+
+python test.py $other_args -r $r > "output_files/$file$end"

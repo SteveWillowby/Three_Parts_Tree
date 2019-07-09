435349a2978dfe869f07b89bf89f339ced6780ab
Steve Willowby <jhibshma@nd.edu>
2019-07-09 14:47:58 -0400

    Has dos issues in bash file.

--- progressive_edge_replacement.sh
+++ progressive_edge_replacement.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 r=0.0
-increment=0.2
+increase=2.0
 
 array=( $@ )
 len=${#array[@]}
@@ -10,29 +10,54 @@ other_args=${array[@]:0:$len-1}
 
 end="_r=$r"
 
-time python test.py $other_args -r $r > "output_files/$file$end"
+{ time python test.py $other_args -r $r; } &> "output_files/$file$end"
 
-r=$(echo $r + $increment | bc)
+r=0.0025
 end="_r=$r"
 
-time python test.py $other_args -r $r > "output_files/$file$end"
+{ time python test.py $other_args -r $r; } &> "output_files/$file$end"
 
-r=$(echo $r + $increment | bc)
+r=$(echo "$(($r * $increase))" | bc -l)
 end="_r=$r"
 
-time python test.py $other_args -r $r > "output_files/$file$end"
+{ time python test.py $other_args -r $r; } &> "output_files/$file$end"
 
-r=$(echo $r + $increment | bc)
+r=$(echo "$(($r * $increase))" | bc -l)
 end="_r=$r"
 
-time python test.py $other_args -r $r > "output_files/$file$end"
+{ time python test.py $other_args -r $r; } &> "output_files/$file$end"
 
-r=$(echo $r + $increment | bc)
+r=$(echo "$(($r * $increase))" | bc -l)
 end="_r=$r"
 
-time python test.py $other_args -r $r > "output_files/$file$end"
+{ time python test.py $other_args -r $r; } &> "output_files/$file$end"
 
-r=$(echo $r + $increment | bc)
+r=$(echo "$(($r * $increase))" | bc -l)
 end="_r=$r"
 
-time python test.py $other_args -r $r > "output_files/$file$end"
+{ time python test.py $other_args -r $r; } &> "output_files/$file$end"
+
+r=$(echo "$(($r * $increase))" | bc -l)
+end="_r=$r"
+
+{ time python test.py $other_args -r $r; } &> "output_files/$file$end"
+
+r=$(echo "$(($r * $increase))" | bc -l)
+end="_r=$r"
+
+{ time python test.py $other_args -r $r; } &> "output_files/$file$end"
+
+r=$(echo "$(($r * $increase))" | bc -l)
+end="_r=$r"
+
+{ time python test.py $other_args -r $r; } &> "output_files/$file$end"
+
+r=$(echo "$(($r * $increase))" | bc -l)
+end="_r=$r"
+
+{ time python test.py $other_args -r $r; } &> "output_files/$file$end"
+
+r=1.0
+end="_r=$r"
+
+{ time python test.py $other_args -r $r; } &> "output_files/$file$end"

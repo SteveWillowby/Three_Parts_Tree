22f3058ac6ad04cec0b2feeb08d3fa6abee77714
Steve Willowby <justus.i.hibshman@gmail.com>
2019-07-19 15:49:17 -0400

    Some copied code which could be modified to work for digraphs.

--- test_utils.py
+++ test_utils.py
@@ -85,3 +85,49 @@ def relabel_nodes(G):
 
     return G_prime
         
+# This code is unfinished. It was copied from a place that used it for UNDIRECTED graphs.
+def make_graph_with_same_degree_dist(G):
+    G_sequence = list(d for n, d in G.degree())
+    G_sequence.sort()
+    sorted_G_sequence = list((d, n) for n, d in G.degree())
+    sorted_G_sequence.sort(key=lambda tup: tup[0])
+    done = False
+    while not done:
+        G_prime = nx.configuration_model(G_sequence)
+        G_prime = nx.Graph(G_prime)
+        G_prime.remove_edges_from(G_prime.selfloop_edges())
+        tries = 10
+        while tries > 0 and (len(G.edges()) != len(G_prime.edges())):
+            sorted_G_prime_sequence = list((d, n) for n, d in G_prime.degree())
+            sorted_G_prime_sequence.sort(key=lambda tup: tup[0])
+            #print("Sorted G_sequence:")
+            #print(sorted_G_sequence)
+            #print("Sorted G_prime_sequence:")
+            #print(sorted_G_prime_sequence)
+            missing = []
+            for i in range(0, len(G.nodes())):
+                while sorted_G_sequence[i][0] > sorted_G_prime_sequence[i][0]:
+                    missing.append(sorted_G_prime_sequence[i][1])
+                    sorted_G_prime_sequence[i] = (sorted_G_prime_sequence[i][0] + 1, sorted_G_prime_sequence[i][1])
+            missing = np.random.permutation(missing)
+            if len(missing) % 2 != 0:
+                print("Sanity issue! Alert!")
+            #print("Edges before:")
+            #print(G_prime.edges())
+            #print("Missing:")
+            #print(missing)
+            for i in range(0, int(len(missing) / 2)):
+                G_prime.add_edge(missing[2*i], missing[2*i + 1])
+            G_prime = nx.Graph(G_prime)
+            G_prime.remove_edges_from(G_prime.selfloop_edges())
+            #print("Edges after:")
+            #print(G_prime.edges())
+            #if not is_connected(G_prime):
+                #print("Bad: G_prime disconnected")
+            tries -= 1
+        if not is_connected(G_prime):
+            pass
+        elif len(G.edges()) == len(G_prime.edges()):
+            #print("Graph creation successful")
+            done = True
+    return G_prime

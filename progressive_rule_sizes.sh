c6bb3106e4ace30e3173af0e31e905ec52ffe1b5
Steve Willowby <jhibshma@nd.edu>
2019-07-25 14:12:15 -0400

    Up to size 8

--- progressive_rule_sizes.sh
+++ progressive_rule_sizes.sh
@@ -36,3 +36,8 @@ k_max=$((k_max+1))
 end="_$k_min$k_max"
 
 { time python test.py $k_min $k_max $other_args; } &> "output_files/$file$end"
+
+k_max=$((k_max+1))
+end="_$k_min$k_max"
+
+{ time python test.py $k_min $k_max $other_args; } &> "output_files/$file$end"
